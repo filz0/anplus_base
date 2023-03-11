e17965d2712cdd221078951207e1452100bdc859
filz0 <bartex35@vp.pl>
2023-03-11 01:15:13 +0100

    A hotfix to:
    [ANPlus [BASE] | Invasion Update] lua/autorun/addnpcplus_sh_meta.lua:272: attempt to call method 'ANPlusInDeathAnim' (a nil value)
    1. ANPlusAlive - lua/autorun/addnpcplus_sh_meta.lua:272
    2. v - lua/autorun/anp_cup.lua:1801
    3. unknown - lua/includes/modules/hook.lua:96 (x1471)

--- lua/autorun/addnpcplus_sh_meta.lua
+++ lua/autorun/addnpcplus_sh_meta.lua
@@ -110,21 +110,18 @@ end
 ]]--\\\\\\\\\\\\\\\\\\\\\\\\
 
 function metaENT:ANPlusTranslateSequence(anim)
-
 	if isstring( anim ) then
 		local result = self:GetSequenceActivity( self:LookupSequence( anim ) )	
 		if result == nil || result == -1 then	
 			return false			
 		else		
 			return result			
-		end
-		
+		end		
 	elseif isnumber( anim ) then 	
 		return anim		
 	else	
 		return false		
-	end
-	
+	end	
 end
 
 --[[////////////////////////
@@ -269,7 +266,7 @@ end
 ]]--\\\\\\\\\\\\\\\\\\\\\\\\
 
 function metaENT:ANPlusAlive()
-	if IsValid(self) && ( ( self:IsNPC() && !self.m_bDeathAnimPlay && ( ( (SERVER) && self:GetNPCState() != 7 ) || ( (CLIENT) && self:Health() > 0 ) ) ) || ( !self:IsNPC() && self:Health() > 0 ) || ( self:IsPlayer() && self:Alive() ) ) then
+	if IsValid(self) && ( ( self:IsNPC() && ( ( (SERVER) && !self:ANPlusInDeathAnim() && self:GetNPCState() != 7 ) || ( (CLIENT) && self:Health() > 0 ) ) ) || ( !self:IsNPC() && self:Health() > 0 ) || ( self:IsPlayer() && self:Alive() ) ) then
 		return true		
 	else	
 		return false		
